f91b4bc3c9bbbe1643f1611b68a017a6e3263f00
Quoc Ky <quockinguyen.lv@gmail.com>
2025-12-23 03:16:11 +0000

    print page table

--- xv6/kernel/exec.c
+++ xv6/kernel/exec.c
@@ -128,6 +128,13 @@ exec(char *path, char **argv)
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
 
+  //print page table when user want to do
+  //this means page table is printed if pid == 1 (first proc)
+  if(p->pid == 1)
+  {
+    vmprint(p->pagetable);
+  }
+
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
